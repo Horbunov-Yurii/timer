7f75bcf4b327403ced890d2eb4cb7c7aef256438
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-10-06 18:52:22 +0300

    Lev

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 // Завдання 1
 
 // Створити таймер, який буде починати відлік з 1 години та зменшувати час кожну хвилину. При досягненні 30 хвилин, таймер повинен відправляти повідомлення екран про те, що залишилось менше половини часу.
+
+import { alert,error, defaultModules } from "@pnotify/core";
+import * as PNotifyMobile from "@pnotify/mobile";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/mobile/dist/PNotifyMobile.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
+
+defaultModules.set(PNotifyMobile, {});
+
+
 let time = 60;
 
 const id = setInterval(() => {
     time -= 1;
     console.log("Виклик функції" + " " + time);
     if (time === 30) {
-        alert("Залишилось менше половини часу");
+alert({text: 'Час вийде через 30 секунд', delay: 1000});
     } else if (time === 0) {
         alert("Час вийшов");
         console.log("Виклик функції" + " " + time);
